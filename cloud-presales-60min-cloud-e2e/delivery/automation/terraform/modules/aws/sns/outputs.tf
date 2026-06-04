a401eecef7e0ae0154268a9ed646ca5370789a73
Shawn Siram <207046+ksiram@users.noreply.github.com>
2026-06-04 15:20:12 -0500

    Remove ISB cloud e2e test artifact

--- cloud-presales-60min-cloud-e2e/delivery/automation/terraform/modules/aws/sns/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "topic_arn" {
-  description = "SNS topic ARN"
-  value       = aws_sns_topic.ops_alerts.arn
-}
-
-output "topic_name" {
-  description = "SNS topic name"
-  value       = aws_sns_topic.ops_alerts.name
-}

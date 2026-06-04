d7bce7b58073a83de5ca24bb5a1ee7c8a48005f0
Shawn Siram <207046+ksiram@users.noreply.github.com>
2026-06-04 15:20:16 -0500

    Remove ISB cloud e2e test artifact

--- cloud-presales-60min-cloud-e2e/delivery/automation/terraform/modules/aws/waf/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "web_acl_arn" {
-  description = "WAF WebACL ARN"
-  value       = aws_wafv2_web_acl.main.arn
-}
-
-output "web_acl_id" {
-  description = "WAF WebACL ID"
-  value       = aws_wafv2_web_acl.main.id
-}

180ffdf8957e9a575f75a3f8bd2b8d1a7a1e0862
Shawn Siram <207046+ksiram@users.noreply.github.com>
2026-06-04 15:20:10 -0500

    Remove ISB cloud e2e test artifact

--- cloud-presales-60min-cloud-e2e/delivery/automation/terraform/modules/aws/secrets-manager/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "github_pat_secret_arn" {
-  description = "GitHub PAT Secrets Manager secret ARN"
-  value       = aws_secretsmanager_secret.github_pat.arn
-}
-
-output "github_pat_secret_name" {
-  description = "GitHub PAT Secrets Manager secret name"
-  value       = aws_secretsmanager_secret.github_pat.name
-}

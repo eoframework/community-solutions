9407db3f9c0fc4f1531243d550454f26a508a268
Shawn Siram <207046+ksiram@users.noreply.github.com>
2026-06-05 14:14:58 -0500

    ISB: 1db81e21-1605-480b-8672-226127aa56a3 — add automation/terraform/modules/networking/outputs.tf

---
+++ amatra-isb-e2e-retest/automation/terraform/modules/networking/outputs.tf
@@ -0,0 +1,29 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr" {
+  description = "VPC CIDR"
+  value       = module.vpc.vpc_cidr
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnet_ids
+}
+
+output "database_subnet_ids" {
+  description = "Database subnet IDs"
+  value       = module.vpc.database_subnet_ids
+}
+
+output "vpc_endpoint_sg_id" {
+  description = "VPC endpoint security group ID"
+  value       = module.vpc.vpc_endpoint_sg_id
+}

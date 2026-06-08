78355c6bd497e8a63d9990b018dd6029490da507
Shawn Siram <207046+ksiram@users.noreply.github.com>
2026-06-08 13:22:53 -0500

    Remove ISB E2E test publish: amatra-intelligent-solution-builder

--- amatra-intelligent-solution-builder/delivery/automation/terraform/modules/aws/cloudtrail/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "trail_arn" {
-  description = "CloudTrail trail ARN"
-  value       = var.enabled ? aws_cloudtrail.main[0].arn : ""
-}
-
-output "trail_name" {
-  description = "CloudTrail trail name"
-  value       = var.enabled ? aws_cloudtrail.main[0].name : ""
-}
--- amatra-intelligent-solution-builder/delivery/automation/terraform/modules/aws/guardduty/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "detector_id" {
-  description = "GuardDuty detector ID"
-  value       = var.enabled ? aws_guardduty_detector.main[0].id : ""
-}

da62326843f29ca6ebdbd32bb69b2bfcd127dad8
Shawn Siram <207046+ksiram@users.noreply.github.com>
2026-06-08 13:24:04 -0500

    Remove amatra-isb-e2e-retest/automation/terraform/modules/aws/sns/outputs.tf (ISB test cleanup)

--- amatra-isb-e2e-retest/automation/terraform/modules/aws/sns/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "topic_arn" {
-  description = "SNS topic ARN"
-  value       = aws_sns_topic.main.arn
-}
-
-output "topic_name" {
-  description = "SNS topic name"
-  value       = aws_sns_topic.main.name
-}

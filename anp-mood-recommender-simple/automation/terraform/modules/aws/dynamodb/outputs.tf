f7388f291f753bd1ae6f76999506e60a04b3f889
Shawn Siram <207046+ksiram@users.noreply.github.com>
2026-06-08 20:57:34 -0500

    ISB: 14c0ed42-7158-42aa-a84d-3b6cc078b93c — add automation/terraform/modules/aws/dynamodb/outputs.tf

---
+++ anp-mood-recommender-simple/automation/terraform/modules/aws/dynamodb/outputs.tf
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------------------
+# AWS DynamoDB Table - Outputs
+#------------------------------------------------------------------------------
+
+output "table_name" {
+  description = "DynamoDB table name"
+  value       = aws_dynamodb_table.this.name
+}
+
+output "table_arn" {
+  description = "DynamoDB table ARN"
+  value       = aws_dynamodb_table.this.arn
+}
+
+output "table_id" {
+  description = "DynamoDB table ID"
+  value       = aws_dynamodb_table.this.id
+}

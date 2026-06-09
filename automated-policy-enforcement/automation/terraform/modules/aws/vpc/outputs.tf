a54341dbc64087e8e5d7f38376851c4cac1e819d
Shawn Siram <207046+ksiram@users.noreply.github.com>
2026-06-09 16:43:17 -0500

    ISB: aa626c42-938d-4d21-bdf7-3848936a84a1 — add automation/terraform/modules/aws/vpc/outputs.tf

---
+++ automated-policy-enforcement/automation/terraform/modules/aws/vpc/outputs.tf
@@ -0,0 +1,39 @@
+output "vpc_id" {
+  description = "ID of the inspection VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "IDs of public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "tgw_subnet_ids" {
+  description = "IDs of Transit Gateway attachment subnets"
+  value       = aws_subnet.tgw[*].id
+}
+
+output "firewall_subnet_ids" {
+  description = "IDs of Network Firewall subnets"
+  value       = aws_subnet.firewall[*].id
+}
+
+output "nat_gateway_ids" {
+  description = "IDs of NAT Gateway instances"
+  value       = aws_nat_gateway.main[*].id
+}
+
+output "private_route_table_ids" {
+  description = "IDs of private route tables"
+  value       = aws_route_table.private[*].id
+}

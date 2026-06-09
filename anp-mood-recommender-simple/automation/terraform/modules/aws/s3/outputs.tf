3441329469a719b02fafcc63e3a17a36c80987ae
Shawn Siram <207046+ksiram@users.noreply.github.com>
2026-06-08 20:57:39 -0500

    ISB: 14c0ed42-7158-42aa-a84d-3b6cc078b93c — add automation/terraform/modules/aws/s3/outputs.tf

---
+++ anp-mood-recommender-simple/automation/terraform/modules/aws/s3/outputs.tf
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------------------
+# AWS S3 Bucket - Outputs
+#------------------------------------------------------------------------------
+
+output "bucket_id" {
+  description = "S3 bucket name / ID"
+  value       = aws_s3_bucket.this.id
+}
+
+output "bucket_arn" {
+  description = "S3 bucket ARN"
+  value       = aws_s3_bucket.this.arn
+}
+
+output "bucket_regional_domain_name" {
+  description = "Regional domain name of the S3 bucket"
+  value       = aws_s3_bucket.this.bucket_regional_domain_name
+}

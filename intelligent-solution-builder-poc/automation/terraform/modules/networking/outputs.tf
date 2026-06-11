757e1358f8e43b517b1a04eb698a8e6141f71ee1
Shawn Siram <207046+ksiram@users.noreply.github.com>
2026-06-11 10:03:48 -0500

    ISB: e00ecd4c-e350-4cc2-b45b-56c3e496a776 — add automation/terraform/modules/networking/outputs.tf

---
+++ intelligent-solution-builder-poc/automation/terraform/modules/networking/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnet_ids
+}
+
+output "lambda_security_group_id" {
+  description = "Security group ID for Lambda"
+  value       = module.vpc.lambda_security_group_id
+}
